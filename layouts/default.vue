893f382d2e399e9745e2426051e798118d864ff7
Sugar_F0x <sugarfox.dev@gmail.com>
2020-12-25 16:37:00 +0300

    :art: Setup clipboard & minified displays
    > Clipboard-like display is for PC view
    > Minified view is automatically set when opened on phone

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,44 @@
 <template lang="pug">
-  v-app(dark)
-    v-app-bar(app)
-      router-link(
-        :to="localePath('/')"
-        style="text-decoration: none; color: inherit;"
-      )
-        v-row(
-          justify="center"
-          align="center"
-        )
-          v-toolbar-title.ml-3 НТП
-    v-main
-      v-container
-        nuxt
-    v-footer(
-      app
-      absolute
+  v-app.app
+    v-row(
+      align="start"
+      justify="center"
+      no-gutters
     )
-      v-spacer
-      span Sugar Archives Template &copy; {{ new Date().getFullYear() }}
+      v-col.col(
+        cols="12"
+        sm="10"
+        md="8"
+        lg="7"
+        xl="6"
+      )
+        v-card.ma-sm-10.pa-sm-10.clipboard(elevation="12")
+          v-card.page
+            v-app-bar.headline НТП Деловые консультации
+            v-main
+              v-container
+                nuxt
+            v-footer(absolute app)
+              v-spacer
+              | Power By Sugar &copy; 2020
 </template>
+
+<style lang="sass" scoped>
+.app
+  background-color: #D7DBDB
+  height: 100vh
+.col
+  height: 100%
+  display: flex
+  flex-flow: column
+.clipboard
+  border-radius: 1rem
+  background-color: #080425
+  display: flex
+  flex-flow: column
+  flex-grow: 1
+.page
+  background-color: white
+  border-radius: 0 !important
+  flex-grow: 1
+</style>
