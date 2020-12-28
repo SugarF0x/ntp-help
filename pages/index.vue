60d5b4b8289ea0973471551acce10e88b65dc45a
Sugar_F0x <sugarfox.dev@gmail.com>
2020-12-28 18:44:24 +0300

    :x: Revert previous commit
    Client wanted to get rid of legacy template style
    Quote: "it looks like obituary"

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template lang="pug">
-  article.text-justify
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus hic praesentium quaerat veniam voluptatum. Facere, illo laborum nam quasi quis rem sit unde velit. Ab accusantium alias amet aspernatur doloribus eveniet nihil quaerat, quidem ullam? Architecto commodi cupiditate molestiae ratione saepe. Amet aspernatur aut cum dolores dolorum id impedit, maxime nam nostrum odio quia quod repellendus suscipit veritatis vero! Atque earum explicabo ipsum nesciunt. Ad aspernatur cupiditate delectus distinctio, dolore dolores eaque eius esse et exercitationem explicabo facilis hic necessitatibus nihil non officia praesentium quaerat quas, quisquam quod quos sunt tempora tenetur ullam vel velit, vitae voluptate. Maiores molestiae, nostrum!
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut dicta error harum hic laborum minima molestiae nisi odit ullam voluptate?
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, quia.
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur assumenda dolore, eius expedita fugiat fugit harum iste libero magni perspiciatis possimus quibusdam rem sequi temporibus!
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, alias, amet atque autem consequuntur doloremque dolores doloribus, eius esse ipsa neque nisi quas quod quos reprehenderit unde voluptate. Accusamus animi architecto blanditiis doloremque error exercitationem illum ipsa iusto laborum natus necessitatibus, officiis quae quis ratione repellat soluta tempora totam voluptatem. Aperiam blanditiis corporis, culpa delectus dolor earum eligendi error est excepturi expedita inventore libero maxime odio optio praesentium quae quasi quos, reiciendis repellat sequi suscipit totam ullam ut veritatis?
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, alias, amet atque autem consequuntur doloremque dolores doloribus, eius esse ipsa neque nisi quas quod quos reprehenderit unde voluptate. Accusamus animi architecto blanditiis doloremque error exercitationem illum ipsa iusto laborum natus necessitatibus, officiis quae quis ratione repellat soluta tempora totam voluptatem. Aperiam blanditiis corporis, culpa delectus dolor earum eligendi error est excepturi expedita inventore libero maxime odio optio praesentium quae quasi quos, reiciendis repellat sequi suscipit totam ullam ut veritatis?
-    p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus, alias, amet atque autem consequuntur doloremque dolores doloribus, eius esse ipsa neque nisi quas quod quos reprehenderit unde voluptate. Accusamus animi architecto blanditiis doloremque error exercitationem illum ipsa iusto laborum natus necessitatibus, officiis quae quis ratione repellat soluta tempora totam voluptatem. Aperiam blanditiis corporis, culpa delectus dolor earum eligendi error est excepturi expedita inventore libero maxime odio optio praesentium quae quasi quos, reiciendis repellat sequi suscipit totam ullam ut veritatis?
+  v-row(
+    justify="center"
+    align="center"
+  )
+    v-col(
+      cols="12"
+      sm="8"
+      md="6"
+    )
+      v-card
+        v-card-title.headline Default page for project startup
+        v-card-text This is a generic landing page
 </template>
 
 <script lang="ts">
