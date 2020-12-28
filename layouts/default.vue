832b4ad4ab1e1866d7a8068e252b4c5af079690d
Sugar_F0x <sugarfox.dev@gmail.com>
2020-12-28 20:09:09 +0300

    :white_check_mark: Somewhat settle on default layout
    > Add style to logo section
    > Add basic navigation
    > Add Logo text postfix to the footer
    > Add "Powered By" to the footer
      // I should really make it into a fleshed-out reusable component
      // and just import on demand

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-  v-app(dark)
-    v-app-bar(app)
+  v-app
+    v-app-bar.app-bar(app)
       router-link(
         :to="localePath('/')"
         style="text-decoration: none; color: inherit;"
@@ -9,7 +9,18 @@
           justify="center"
           align="center"
         )
-          v-toolbar-title.ml-3 НТП
+          v-toolbar-title.ml-3.text-h4
+            span(style="color: #DD2C00") НТП
+            span.ml-2.text-h5 Деловые консультации
+      v-spacer
+      v-toolbar-items(
+        v-for="link in links"
+        :key="link.text"
+      )
+        v-btn(
+          nuxt
+          :to="localePath(link.path)"
+        ) {{ link.text }}
     v-main
       v-container
         nuxt
@@ -17,6 +28,44 @@
       app
       absolute
     )
+      span Сложные и нестандартные проекты в области права
       v-spacer
-      span Sugar Archives Template &copy; {{ new Date().getFullYear() }}
+      span
+        | Powered by
+        v-btn.pa-0.mx-1(
+          text
+          href="https://github.com/SugarF0x"
+        ) Sugar
+        | &copy; {{ new Date().getFullYear() }}
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+interface Link {
+  text: string,
+  path: string
+}
+
+export default Vue.extend({
+  name: 'layout-default',
+
+  data() {
+    return {
+      links: [
+        { text: 'Сфера деятельности', path: '/' },
+        { text: 'Новости', path: '/news' },
+        { text: 'Ключевые фигуры', path: '/key-figures' },
+        { text: 'Подразделения', path: '/departments' },
+        { text: 'Из практики', path: '/cases' },
+        { text: 'Контакты', path: '/contacts' },
+      ] as Link[]
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.app-bar > *
+  padding-right: 0 !important // attach nav section to the right
+</style>
